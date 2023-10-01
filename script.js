19c17d8449492b310065df2de3b8e789f232b8b2
Matteo Bennis <thebennasio@gmail.com>
2023-10-01 20:49:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var whenMobile = screen.width;
+if(whenMobile < 1190){
+    let icons = document.querySelectorAll(".socialIcon");
+    icons.forEach(element => {
+        let color = window.getComputedStyle(element).borderColor;
+        element.querySelector('a').querySelector('i').style.color=color;
+        element.style.border="none";
+        element.removeAttribute('onmouseover');
+        element.removeAttribute('onmouseout'); 
+    });
+}
+else{
+    function show(element){
+        let color = window.getComputedStyle(element).borderColor;
+        element.style.border="none";
+        element.style.borderTop="4px solid " + color;
+        element.style.paddingTop="13px"; 
+        element.querySelector('a').querySelector('i').style.color=color;
+    }
+    function unShow(element){
+        let color = window.getComputedStyle(element).borderColor;
+        element.style.border="none";
+        element.style.borderBottom="4px solid " + color;
+        element.querySelector('a').querySelector('i').style.color="lightgrey";
+    }
+}
