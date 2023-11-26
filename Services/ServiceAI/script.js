a42a2b19a786aed83798c41d2185f62c372c0230
Th3DarkOn3 <thebennasio@gmail.com>
2023-11-26 14:47:46 +0100

    update

---
+++ Services/ServiceAI/script.js
@@ -0,0 +1,32 @@
+var whenMobile = screen.width;
+if(whenMobile < 1190){
+    let icons = document.querySelectorAll(".socialIcon");
+    icons.forEach(element => {
+        let color = window.getComputedStyle(element).borderColor;
+        element.querySelector('a').querySelector('i').style.color=color;
+        element.style.border="none";
+        element.removeAttribute('onmouseover');
+        element.removeAttribute('onmouseout'); 
+    });
+}
+else{
+    function show(element){
+        let color = window.getComputedStyle(element).borderColor;
+        element.style.border="none";
+        element.style.borderTop="4px solid " + color;
+        element.style.paddingTop="13px"; 
+        element.querySelector('a').querySelector('i').style.color=color;
+    }
+    function unShow(element){
+        let color = window.getComputedStyle(element).borderColor;
+        element.style.border="none";
+        element.style.borderBottom="4px solid " + color;
+        element.querySelector('a').querySelector('i').style.color="lightgrey";
+    }
+}
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
