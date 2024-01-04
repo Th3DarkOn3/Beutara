d20720ca92623d054c4b64f42dd598c507329be0
Th3DarkOn3 <thebennasio@gmail.com>
2024-01-04 14:22:47 +0100

    update

--- Services/DigitalMarketing/script.js
+++ Services/DigitalMarketing/script.js
@@ -42,3 +42,20 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+    const element = document.getElementById('embed-iframe');
+    const options = {
+      width: '100%',
+      height: '160',
+      uri: 'spotify:album:55jJt2UtILmQmUWeakw4d5'
+    };
+    const callback = (EmbedController) => {
+      document.querySelectorAll('.episode').forEach(
+        episode => {
+          episode.addEventListener('click', () => {
+            EmbedController.loadUri(episode.dataset.spotifyId)
+          });
+        })
+    };
+    IFrameAPI.createController(element, options, callback);
+  };
